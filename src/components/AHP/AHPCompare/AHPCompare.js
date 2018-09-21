ccf13d364d7af334c93adb685f4f86dde17f0fc8
Dima Kurilenko <golah30@gmail.com>
2018-09-21 19:20:43 +0300

    ahp branch

--- src/components/AHP/AHPCompare/AHPCompare.js
+++ src/components/AHP/AHPCompare/AHPCompare.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
-import { Button, TextInput } from '../../UI';
+
 import { setAhpTarget } from '../../../ducks/AHP';
 
 class AHPCompare extends React.PureComponent {
